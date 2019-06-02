8cc0e155256ae95548ed487b6927efae4a8a32be
ziihh <zainb@stud.ntnu.no>
2019-06-02 15:27:30 +0200

    Cleaned up unnecessary files and code.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,18 @@
 import React from "react";
 import { BrowserRouter, Route, Link } from "react-router-dom";
-import logo from "./logo.svg";
-import Home from "./Home/Home"
+import Home from "./components/Home/Home";
 import "./App.css";
 
 class App extends React.Component {
-
-    render() {
-
-        return(
-           <BrowserRouter>
-                <div className="body">
-                    <Route path = "/" component = {Home} />
-                </div>
-
-           </BrowserRouter>
-        )
-    }
-
+	render() {
+		return (
+			<BrowserRouter>
+				<div className="body">
+					<Route path="/" component={Home} />
+				</div>
+			</BrowserRouter>
+		);
+	}
 }
-
 
 export default App;
